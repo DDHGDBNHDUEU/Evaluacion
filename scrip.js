df07eebc72fc3e5147d2f290cbcaecafe9d82ad7
verflou <88504966+verflou@users.noreply.github.com>
2023-05-09 11:37:41 -0300

    hola

--- scrip.js
+++ scrip.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const port = 2500
+const password = 'mayra2007';
+const uri ='mongodb+srv://May: ${password} @tarea.ukuqqcc.mongodb.net/?retryWrites=true&w=majority';
 
-const electrodomesticosSchema = new mongoose.Schema({
-    tipo: { type: String, required: "decir que electrodomestico es"},
-    origen: { type: String, required: "decir donde fue fabricado"},
-    precio: { type: Number, required:"decir cuanto vale" },
-    stock: { type: Number, required: "decir la cantida de stock" },
-  });
+app.listen(port, () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
+});
+
+mongoose.connect( uri ,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}
+);
